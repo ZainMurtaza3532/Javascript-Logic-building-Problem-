477a21652f60f3267fb27ba9b8ebdffb97392e45
Zain Murtaza <zainmurtaza3532@gmail.com>
2025-12-10 04:29:42 +0500

    Solve the Sumof2Numbers Problem

---
+++ Day1/Sumof2Numbers.js
@@ -0,0 +1,13 @@
+// let a = 10;
+// let b = 20;
+
+// sum = a + b;
+// console.log("The sum of", a, "and", b, "is", sum);
+
+function sum(x,y){
+    const sum = x + y;
+    console.log(sum)
+}
+sum(5,15);
+sum(25,35);
+sum(100,200);
