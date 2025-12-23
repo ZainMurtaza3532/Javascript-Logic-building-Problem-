db446da6820212bedf63b67a69538e763fde1c24
Zain Murtaza <zainmurtaza3532@gmail.com>
2025-12-24 04:00:46 +0500

    reverse string problem

---
+++ Day2/reverseAtstring.js
@@ -0,0 +1,7 @@
+function reverseString(str) {
+  if (str === "") return "";
+  return reverseString(str.slice(1)) + str[0];
+}
+
+console.log(reverseString("JavaScript"));
+console.log(reverseString("Hello, World!"));
