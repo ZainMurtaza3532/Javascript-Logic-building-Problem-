57d159a6e1ad75886cb1c6f5a27531a1c0ba4b20
Zain Murtaza <zainmurtaza3532@gmail.com>
2025-12-23 23:56:46 +0500

    addevenodd number problem solve

---
+++ Day2/oddEven.js
@@ -0,0 +1,12 @@
+function checkEvenOdd(number) {
+    if (number % 2 === 0) {
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
+
+console.log(checkEvenOdd(4));
+console.log(checkEvenOdd(7));
+console.log(checkEvenOdd(0));
+console.log(checkEvenOdd(-3));
